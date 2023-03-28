5b7bbb28d31230114efe84e572aadac9a888844f
Wille Salomonsson <wisa0917@storsjogymnasiet.se>
2023-03-28 14:41:37 +0200

    försöker att lära mig hur onclick detect funkar

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-let siffra = 0;
+/*let siffra = 0;
 
 function plusClick(){
 console.log("plusClick");
 siffra = siffra + 1;
 
 document.getElementById("taltext").innerHTML = siffra;
-
 }
+*/
+document.getElementById("taltext").addEventListener("click", function(){
+    alert("click")
+});
+      
+    
 
+/*
 function minusClick(){
 console.log("minusClick");
 siffra = siffra -1;
 
 document.getElementById("taltext").innerHTML = siffra;
 }
+*/
