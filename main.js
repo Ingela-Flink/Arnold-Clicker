62385ea2f11a8c47282b65e3620ce4de4aee8092
Wille Salomonsson <wisa0917@storsjogymnasiet.se>
2023-04-25 16:37:48 +0200

    JAG har ingen som helst jävla aning vrf det står cannot set properties of null..!!

--- main.js
+++ main.js
@@ -1,47 +1,33 @@
-/*let siffra = 0;
+let siffra = 0;
+document.getElementById("taltext").innerHTML = siffra;
 
 function plusClick(){
 console.log("plusClick");
 siffra = siffra + 1;
-
-document.getElementById("taltext").innerHTML = siffra;
-}
-*/
-
-/*function plusClick(){
-  console.log("ARNOLD");
-  var x = event.clientX;
-  var y = event.clientY;
-  var bild = document.getElementById("bicep");
-  bild.style.display = 'block';
-  bild.style.position = 'absolute';
-  bild.style.left = x + 'px';
-  bild.style.top = y + 'px';
-setTimeout(fade_out, 3000); 
-}
-
-var fade_out = function() {
-    bicep.style.display = "none";
-  }
-*/
-function plusClick(){
 var image = new Image();
 var x = event.clientX;
 var y = event.clientY;
+var fade_out = function() {
+  image.style.display = "none";
+}
 image.src = 'img/Bicep.png';
 image.style.width = '49px';
 image.style.height = '40px';
 image.style.transform = 'translate(-50%, -50%)';
-image.style.userSelect = 'none';
 image.style.display = 'block';
 image.style.position = 'absolute';
 image.style.left = x + 'px';
 image.style.top = y + 'px';
-document.body.appendChild(image);
+setTimeout(fade_out, 3000)
+document.body.appendChild(image); 
 }
-  
 
-  
+function plusEttClick(){
+
+  if (siffra > 9) {
+    siffra = siffra - 10;
+  } else {alert('FAN')}
+}
 
 
 
